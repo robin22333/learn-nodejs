dc37b2758bddecb9f3381a9e04c19c689951ad90
robin <luobin1105@qq.com>
2015-10-13 23:57:13 +0800

    use test frame

---
+++ use-test-frame/main.js
@@ -0,0 +1,18 @@
+var fibonacci = function (n) {
+    if (n === 0) {
+        return 0;
+    }
+    if (n === 1) {
+        return 1;
+    }
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
+if (require.main === module) {
+    //如果直接进入 main.js，进入此处
+    //如果 main.js 被其它文件 require,则不会执行
+    var n = Number(process.argv[2]);
+    console.log('fibonacci(' + n + ') is ', fibonacci(n));
+}
+
+exports.fibonacci = fibonacci;
